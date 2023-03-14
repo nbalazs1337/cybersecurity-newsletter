fc32cc89699ef7b83b17a8bb11fe40201fcf7fe5
negrea_balazs <62569008+nbalazs1337@users.noreply.github.com>
2023-03-14 10:02:00 +0200

    design done

--- my_env/main/feedly_utils.py
+++ my_env/main/feedly_utils.py
@@ -3,8 +3,8 @@ import requests
 
 
 def get_feedly_data():
-    #stream_id = 'feed/http://rss.packetstormsecurity.org/news/tags/zero_day/'  
-    stream_id = 'feed/https://advisories.feedly.com/google/chrome/feed.json'
+    stream_id = 'feed/http://rss.packetstormsecurity.org/news/tags/zero_day/'  
+    #stream_id = 'feed/https://advisories.feedly.com/google/chrome/feed.json'
     count = 3
     url = f"https://cloud.feedly.com/v3/streams/contents?streamId={stream_id}&count={count}"
     access_token = "A8JCTCabGqCwwF3bAf2j_xHaE3fx52K_12RZ-fHzNfes0JGW5xvoYHTV7XBMmBhQ0noppF-pdgI4dExKDEUT0Mda4QhNUVlB5TjyRV6d1c-4NNCCgi75-Uhk_q2uw4Ar5X_rQsvipPNAtciO9rtm-LmP5AuvkKhEqYU4B0TyaCxqlO_gvfzuTLAhacfmHYwReFeOxKN4qfmoPoyGPHHn34KVuRw0O6o8kFoGAQUR3eR7wuq_W9XlXfxdj18:feedlydev"
