c5842ee0b6714cd8c6244238b3780cb8bd91022a
HoreaR <103374962+HoreaR@users.noreply.github.com>
2023-03-27 10:43:25 +0300

    rtf

--- my_env/main/feedly_utils.py
+++ my_env/main/feedly_utils.py
@@ -43,10 +43,6 @@ def send_newsletter(recipient_list, data):
 
     # Render the newsletter template with data from Feedly API
     html_message = render_to_string('my_template.html', {'results': data})
-<<<<<<< Updated upstream
-   
-=======
->>>>>>> Stashed changes
     plain_message = "Your Feedly Newsletter"
 
     # create an instance of the SES client
